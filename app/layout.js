fe672f7b2953b0601925328cce43f2783a818990
shaktisingh647 <consolelogshakti@gmail.com>
2025-12-06 11:49:45 +0530

    layout_changes

--- app/layout.js
+++ app/layout.js
@@ -23,7 +23,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <Navbar />
-        {children}
+        <main className="relative overflow-hidden">
+          {children}
+        </main>
         <Footer/>
       </body>
     </html>
