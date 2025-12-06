62eec2166f60e24db0c5e142eb459e4b6794d677
shaktisingh647 <consolelogshakti@gmail.com>
2025-12-06 12:06:40 +0530

    config

---
+++ components/Navbar.jsx
@@ -0,0 +1,10 @@
+
+const Navbar = () => {
+  return (
+    <nav className="border-2 border-red-500 flex flex-between max-container padding-container relative z-30 py-5">
+heyiiiii
+    </nav>
+  )
+}
+
+export default Navbar
