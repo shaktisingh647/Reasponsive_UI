fed01e22479b0559672d339ad93bcd4ccd9ad0c7
shaktisingh647 <consolelogshakti@gmail.com>
2025-12-08 14:58:54 +0530

    changes

--- app/page.js
+++ app/page.js
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-     <h1 className="text-bold text-teal-500">Hi this is a basefile</h1>
+     <h1 className="text-bold text-teal-500">Hi </h1>
   );
 }
